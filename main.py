9e34e2f3a923487bc8c5c3f1f7952bd0bb450fbf
Lee2907 <kamvelihle.buka2907@gmail.com>
2021-05-31 13:43:46 +0200

    commit

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 marks = [70,45,90,12]
 names = ["Memphis","Godwin","Thando","Thabo"]
 
-names = np.array(["Memphis","Godwin","Thando","Thabo"])
-marks = np.array([70,45,90,12])
+names = np.array(names)
+marks = np.array(marks)
 plt.bar(names,marks)
+plt.xlabel("Names of Students")
+plt.ylabel("Marks of Students")
 plt.show()
 
 
